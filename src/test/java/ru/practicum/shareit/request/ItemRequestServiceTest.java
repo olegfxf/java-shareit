1003de7c5920a8cf92e43098f174dda385b8349d
Oleg <ovasilyevfx@mail.ru>
2023-05-21 15:13:30 +0300

    fix:test

--- src/test/java/ru/practicum/shareit/request/ItemRequestServiceTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestServiceTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.exceptions.UserNotFoundException;
 import ru.practicum.shareit.item.ItemRepository;
 import ru.practicum.shareit.item.model.Item;
